6501b72c0adbc30d23aa62e96a45fd80e6b9e827
Gabriel John Gagno <gjpgagno@gmail.com>
2016-04-29 18:08:34 +0800

    added card link api

--- web/index.php
+++ web/index.php
@@ -38,6 +38,7 @@ $app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->get('/hello', 'App\\Controllers\\HelloController::hello')->before('App\\Middleware\\OAuthMiddleware::handle');
 $app->get('/users', 'App\\Controllers\\HelloController::get')->before('App\\Middleware\\OAuthMiddleware::handle');
 $app->post('/requestfee', 'App\\Controllers\\RequestFeeController::requestFee')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->post('/cardlink', 'App\\Controllers\\CardLinkController::cardlink')->before('App\\Middleware\\OAuthMiddleware::handle');
 $app->mount('/oauth', new App\Libraries\OAuth2Library());
 
 
