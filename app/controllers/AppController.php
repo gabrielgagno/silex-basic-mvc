3fa779c3995eba2710fdf6220e3c217decaed117
Gabriel John Gagno <gjpgagno@gmail.com>
2016-05-03 15:56:42 +0800

    removed old controllers and consolidated functions into one controller added response handler

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -10,6 +10,7 @@ namespace App\Controllers;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Unirest\Request as CurlRequest;
 
 
@@ -17,85 +18,139 @@ class AppController
 {
     public function cardLink(Application $app, Request $request)
     {
-        // TODO put validation here
         $p2meResponse = CurlRequest::get("http://localhost:3000/cardlink", null, null);
-        $response = array(
-            'status' => 10,
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
     public function requestFee(Request $request, Application $app)
     {
-        // TODO validation procedure
         $p2meResponse = CurlRequest::get("http://localhost:3000/requestfees", null, null);
-        $response = array(
-            'status' => "success",
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
     public function resetOtp(Application $app, Request $request)
     {
         $p2meResponse = CurlRequest::get("http://localhost:3000/resetOTP", null, null);
-        $response = array(
-            'status' => 10,
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
     public function reverse(Application $app, Request $request)
     {
         $p2meResponse = CurlRequest::get("http://localhost:3000/reverse", null, null);
-        $response = array(
-            'status' => 10,
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
     public function topUp(Application $app, Request $request)
     {
         $p2meResponse = CurlRequest::get("http://localhost:3000/topup", null, null);
-        $response = array(
-            'status' => 10,
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
     public function transactionInquiry(Application $app, Request $request)
     {
         $p2meResponse = CurlRequest::get("http://localhost:3000/transactioninquiry", null, null);
-        $response = array(
-            'status' => 10,
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
     public function validateMobileNumber(Application $app, Request $request)
     {
         $p2meResponse = CurlRequest::get("http://localhost:3000/validatemobilenumber", null, null);
-        $response = array(
-            'status' => 10,
-            'timestamp' => date('Y-m-d H:i:s'),
-            'p2me_result'   => $p2meResponse->body
+        $response = $this->handleResponse($p2meResponse);
+        return new Response(
+            json_encode($response),
+            $p2meResponse->code,
+            array(
+                'Content-type'  => 'application/json'
+            )
         );
-        return $app->json($response);
     }
 
-    private function handleResponse()
+    private function handleResponse($p2meResponse)
     {
-        
+
+        $code = $p2meResponse->code;
+        $status = "fail";
+        $message = null; // TODO replace all messages with simple p2meResponse->body later
+        switch($code) {
+            case 201:
+                $message = "No Content";
+                $status = "success";
+                break;
+            case 200:
+                $message = $p2meResponse->body;
+                $status = "success";
+                break;
+            case 400:
+                $message = "Bad Request";
+                break;
+            case 401:
+                $message = "Unauthorized";
+                break;
+            case 403:
+                $message = "Forbidden";
+                break;
+            case 404:
+                $message = "Not Found";
+                break;
+            case 500:
+                $message = "Internal Server Error";
+                break;
+            case 502:
+                $message = "Bad Gateway";
+                break;
+            case 503:
+                $message = "Service Unavailable";
+                break;
+        }
+        $response = array(
+            "status"        => $status,
+            "timestamp"     => date("Y-m-d H:i:s"),
+            "p2me_result"   => $message
+        );
+
+        return $response;
     }
 }
