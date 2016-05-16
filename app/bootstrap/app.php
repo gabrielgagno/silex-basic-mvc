382ec84ff3aa8167b41cafdc8d1fb541004e0c15
Gabriel John Gagno <gjpgagno@gmail.com>
2016-05-16 10:53:12 +0800

    added new bootstrapper file cleaned up index file

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -5,3 +5,51 @@
  * Date: 4/22/16
  * Time: 7:01 PM
  */
+require_once __DIR__.'/../../vendor/autoload.php';
+
+# initialize Silex Application Instance
+$app = new Silex\Application();
+$app->boot();
+
+# register services
+
+# register config service provider for entire app
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/app.php"));
+
+# register config service provder for database
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/database.php"));
+
+# register logger service provider
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../logs/log-'.date('Y-m-d').'.log',
+    'monolog.name' => $app['name']
+));
+
+# register security service provider
+$app->register(new Silex\Provider\SecurityServiceProvider());
+
+# register validator provider (optional)
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+
+# initialize environment here
+try{
+    $app['env'] = new Dotenv\Dotenv(__DIR__.'/../../', '.env.'.$app['environment']);
+    $app['env']->load();
+}
+catch (Exception $e) {
+    $app->json(['error' => 500, 'error_description' => 'Environment Not Found'], 500)->send();
+}
+
+$app['debug'] = \App\Libraries\CoreHelpersLibrary::env('APP_DEBUG', false);
+
+# routes
+$app->get('/requestfee', 'App\\Controllers\\AppController::requestFee')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->post('/cardlink', 'App\\Controllers\\AppController::cardlink')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->post('/validatemobilenumber', 'App\\Controllers\\AppController::validateMobileNumber')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->get('/topup', 'App\\Controllers\\AppController::topUp')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->post('/reverse', 'App\\Controllers\\AppController::reverse')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->get('/transactioninquiry', 'App\\Controllers\\AppController::transactionInquiry')->before('App\\Middleware\\OAuthMiddleware::handle');
+$app->post('/resetotp', 'App\\Controllers\\AppController::resetOtp')->before('App\\Middleware\\OAuthMiddleware::handle');
+
+# oauth routes
+$app->mount('/oauth', new App\Libraries\OAuth2Library());
